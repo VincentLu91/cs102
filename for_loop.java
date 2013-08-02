bd5e81efb9cff34dc5e81dc5a005b1979d0845e9
VincentLu91 <vincentlu@hotmail.ca>
2013-08-02 13:51:29 -0400

    Create for_loop.java
    Sample code for Saylor foundation homework

---
+++ for_loop.java
@@ -0,0 +1,13 @@
+public class forloop
+{ 
+   public static void main(String[] args) 
+   { 
+      System.out.println("This Program will provide you with an illustration of how for-loops can be used");
+
+  System.out.println("We will start with 0 and print out all even numbers until 10");
+for (int count= 0; count <= 10; count+=2) 
+      { 
+         System.out.println(count); 
+      }
+   } 
+}
