2816d2ee029ea2291eba6ca0db0ba04f0614bb96
VincentLu91 <vincentlu@hotmail.ca>
2013-10-15 15:10:17 -0400

    Update RecipeDetailViewController.h

--- RecipeDetailViewController.h
+++ RecipeDetailViewController.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Vincent Lu. All rights reserved.
 //
 
+// This is the interface I wrote after following a tutorial on creating a recipe database app
+// Link: http://www.appcoda.com/objective-c-object-oriented-programming-intro/
 #import <UIKit/UIKit.h>
 
 @interface RecipeDetailViewController : UIViewController
